2f11121c3e3b04ca44b2d56f84396b3620785890
Tokiodan <danibouzidi7@gmail.com>
2026-07-02 11:48:09 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KlantController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Klanten resource routes - RESTful CRUD operations
+    // Requirements: 9.1, 9.2, 9.3
+    Route::resource('klanten', KlantController::class);
 });
 
 require __DIR__.'/auth.php';
