eaa6a42454fb42b655970defac7725d94b4029b1
Marvin <marvinakpabot@gmail.com>
2026-07-02 12:03:35 +0200

    Register medewerker routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MedewerkerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/medewerkers/{medewerker}/verwijderen', [MedewerkerController::class, 'delete'])
+        ->name('medewerkers.delete');
+    Route::resource('medewerkers', MedewerkerController::class)->except(['show']);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
