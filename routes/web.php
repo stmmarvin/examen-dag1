a0721855a3135ca7188fa5d53bc2e87838350b02
Marvin <marvinakpabot@gmail.com>
2026-07-02 12:37:36 +0200

    Redirect homepage to medewerker beheer

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\MedewerkerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('medewerkers.index');
 });
 
 Route::get('/dashboard', function () {
