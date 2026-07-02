4617c8ca82da5264f6ecb2eb16c1447de922d400
Marvin <marvinakpabot@gmail.com>
2026-07-02 11:49:29 +0200

    Add default manager role to users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            // Dag 2 gebruikt rollen om manager-functionaliteit te beveiligen.
+            'rolename' => 'manager',
             'remember_token' => Str::random(10),
         ];
     }
